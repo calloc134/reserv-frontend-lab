ab0baafb12e5e3513de2e755d6373d51228893e2
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-10 18:55:07 +0900

    フック

---
+++ src/hooks/GetWeeklyReservationsHook.tsx
@@ -0,0 +1,66 @@
+import { useAuth } from "@clerk/clerk-react";
+import { reservFetch } from "../utils/reservFetch";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getMondayOfThisWeek } from "../utils/getMondayOfWeek";
+import { convertFromDate } from "../utils/convertFromDate";
+import { ReservationResponseTransformed } from "../types/ReservationResponseTransformed";
+import { convertToDate } from "../utils/convertToDate";
+
+// 内部的にtanstack queryを利用する
+export const GetWeeklyReservationsHook = () => {
+  const { getToken } = useAuth();
+
+  // 呼び出しの形式はYYYY-MM-DD
+  const start_date = getMondayOfThisWeek();
+  const end_date = new Date(
+    start_date.getFullYear(),
+    start_date.getMonth(),
+    start_date.getDate() + 4
+  );
+
+  const raw_start_date = convertFromDate(start_date);
+  const raw_end_date = convertFromDate(end_date);
+
+  const { data, error, isLoading } = useSuspenseQuery({
+    queryKey: ["reservations", "weekly", raw_start_date, raw_end_date],
+    queryFn: async () => {
+      const query_result = (await reservFetch(
+        `/reservations/start_date/${raw_start_date}/end_date/${raw_end_date}/`,
+        {
+          headers: {
+            Authorization: `Bearer ${await getToken()}`,
+          },
+        }
+      )) as {
+        reservations: ReservationResponseTransformed[];
+        start_date: string;
+        end_date: string;
+      };
+
+      const start_date_or = convertToDate(query_result.start_date);
+      if (start_date_or.isErr()) {
+        throw start_date_or.error;
+      }
+      const end_date_or = convertToDate(query_result.end_date);
+      if (end_date_or.isErr()) {
+        throw end_date_or.error;
+      }
+      return {
+        reservations: query_result.reservations.map((x) => {
+          const date_or = convertToDate(x.date);
+          if (date_or.isErr()) {
+            throw date_or.error;
+          }
+          return {
+            ...x,
+            date: date_or.value,
+          };
+        }),
+        start_date: start_date_or.value,
+        end_date: end_date_or.value,
+      };
+    },
+  });
+
+  return { data, error, isLoading };
+};
