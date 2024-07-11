1803b86780e5d076f7476f1444cebfa89755abc7
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-11 16:20:42 +0900

    refactor: 要らないimportの削除

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -1,5 +1,4 @@
 import { UserButton } from "@clerk/clerk-react";
-import { Link } from "@tanstack/react-router";
 
 export const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
