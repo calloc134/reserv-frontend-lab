0f37f5a86bd8ae3a485464361e2de594ca060579
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-11 22:42:32 +0900

    リダイレクト先パスの変更

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 export const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-4">
-      <SignIn fallbackRedirectUrl={"/home"} />
+      <SignIn fallbackRedirectUrl={import.meta.env.VITE_APP_PATH + "/home"} />
     </div>
   );
 };
