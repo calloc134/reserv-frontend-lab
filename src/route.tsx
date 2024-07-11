0a85fa7fac067f56659621fb0079b29f5679fafa
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-11 16:12:09 +0900

    スピナー表示修正

--- src/route.tsx
+++ src/route.tsx
@@ -64,7 +64,15 @@ const home_route = createRoute({
       <HomeLayout>
         <Suspense
           fallback={
-            <div className="flex h-screen justify-center items-center">
+            <div className="flex h-screen justify-top items-center flex-col gap-4">
+              <div className="flex w-1/2  flex-row gap-4 justify-center">
+                <div className="p-2 rounded-lg border-2 border-black w-1/2 text-center bg-gray-200">
+                  予約一覧(週)
+                </div>
+                <div className="p-2 rounded-lg border-2 border-black w-1/2 text-center bg-gray-200">
+                  自分の予約一覧(週)
+                </div>
+              </div>
               <div className="p-16 bg-white rounded-lg col-span-1 sm:col-span-2 md:col-span-2 border-2 border-black">
                 <ClipLoader color="#000" loading={true} size={50} />
               </div>
