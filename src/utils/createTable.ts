1123f600adca20756edfee83b8b99b3fb20dbd13
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-11 12:11:48 +0900

    月をまたぐ処理がうまく動作しない部分を修正

--- src/utils/createTable.ts
+++ src/utils/createTable.ts
@@ -15,14 +15,17 @@ export const createTable = (data: {
     reservations: Array<ReservationResponse[]>;
   }[] = [];
 
-  const date_width = data.end_date.getDate() - data.start_date.getDate() + 1;
+  // 日時の幅を計算
+  // 異なる月同士でも動作するように
+  const date_width =
+    Math.ceil(
+      (data.end_date.getTime() - data.start_date.getTime()) /
+        (1000 * 60 * 60 * 24)
+    ) + 1;
 
   for (let i = 0; i < date_width; i++) {
-    const date = new Date(
-      data.start_date.getFullYear(),
-      data.start_date.getMonth(),
-      data.start_date.getDate() + i
-    );
+    const date = new Date(data.start_date);
+    date.setDate(data.start_date.getDate() + i);
     table.push({
       date: date,
       reservations: [[], [], [], []],
@@ -32,7 +35,10 @@ export const createTable = (data: {
   // 該当する予約を挿入
 
   for (const reservation of data.reservations) {
-    const index = reservation.date.getDate() - data.start_date.getDate();
+    const index = Math.floor(
+      (new Date(reservation.date).getTime() - data.start_date.getTime()) /
+        (1000 * 60 * 60 * 24)
+    );
 
     table[index].reservations[slotToNumber(reservation.slot) - 1].push(
       reservation
