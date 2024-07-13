bd5d05a0803aa1067ed42dec912177fcdb7cc0cc
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-13 12:59:25 +0900

    リダイレクト先修正

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -6,6 +6,9 @@ export const HomeLayout = ({ children }: { children: React.ReactNode }) => {
       <header className="flex items-center justify-between p-4 border-b-4 border-black rounded-b-md bg-slate-200 bg-gradient-to-r from-slate-200 to-slate-400">
         <div className="flex flex-row gap-2 justify-center items-center">
           <h1 className="text-2xl font-bold">部室予約サイト</h1>
+          <a href="/" className="text-sm text-gray-500 hover:text-gray-700">
+            トップページへ
+          </a>
         </div>
         <div>
           <UserButton />
