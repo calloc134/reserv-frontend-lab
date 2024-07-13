27c1dd8709b163017ebe5e0be8569eab439d535a
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-07-13 12:59:35 +0900

    リダイレクト先修正

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,7 +3,13 @@ import { SignIn } from "@clerk/clerk-react";
 export const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-4">
-      <SignIn fallbackRedirectUrl={import.meta.env.VITE_APP_PATH + "/home"} />
+      <SignIn
+        fallbackRedirectUrl={
+          import.meta.env.VITE_APP_PATH
+            ? import.meta.env.VITE_APP_PATH + "/home"
+            : "/home"
+        }
+      />
     </div>
   );
 };
