20798ceb7daf11927aca21b004e4b8e28effbd1c
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-11-14 23:01:01 +0900

    コードの変更とテストの追加

--- src/utils/convert/convertFromDate.ts
+++ src/utils/convert/convertFromDate.ts
@@ -1,6 +1,29 @@
-export function convertFromDate(date: Date): string {
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  return `${year}-${month}-${day}`;
+import { toZonedTime } from "date-fns-tz";
+import { format } from "date-fns";
+
+export function convertFromDate(date: Date) {
+  const time_zone = "Asia/Tokyo";
+  const zoned_date = toZonedTime(date, time_zone);
+  return format(zoned_date, "yyyy-MM-dd");
+}
+
+// タイムゾーンに依存していないことを確かめるためのテスト用関数 ブラジル
+export function convertFromDateInBrasil(date: Date) {
+  const time_zone = "America/Sao_Paulo";
+  const zoned_date = toZonedTime(date, time_zone);
+  return format(zoned_date, "yyyy-MM-dd HH:mm:ss");
+}
+
+// タイムゾーンに依存していないことを確かめるためのテスト用関数 東京
+export function convertFromDateInTokyo(date: Date) {
+  const time_zone = "Asia/Tokyo";
+  const zoned_date = toZonedTime(date, time_zone);
+  return format(zoned_date, "yyyy-MM-dd HH:mm:ss");
+}
+
+// タイムゾーンに依存していないことを確かめるためのテスト用関数 UTC
+export function convertFromDateInUTC(date: Date) {
+  const time_zone = "UTC";
+  const zoned_date = toZonedTime(date, time_zone);
+  return format(zoned_date, "yyyy-MM-dd HH:mm:ss");
 }
---
+++ src/utils/convert/convertToDate.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect } from "vitest";
+import { convertToDate, convertToDateInBrasil } from "./convertToDate";
+
+describe("convertToDate", () => {
+  it("有効な日付文字列の場合はDateオブジェクトを返す ブラジル", () => {
+    const result = convertToDateInBrasil("2024-10-01");
+    expect(result.isOk()).toBe(true);
+    if (result.isOk()) {
+      // この場合、epoch時間は1727751600000となる
+      expect(result.value.getTime()).toBe(1727751600000);
+    }
+  });
+
+  it("有効な日付文字列の場合はDateオブジェクトを返す 東京", () => {
+    const result = convertToDate("2024-10-01");
+    expect(result.isOk()).toBe(true);
+    if (result.isOk()) {
+      // この場合、epoch時間は1727708400000となる
+      expect(result.value.getTime()).toBe(1727708400000);
+    }
+  });
+
+  it("無効な日付文字列の場合はエラーを返す 1", () => {
+    const result = convertToDate("invalid-date-string");
+    expect(result.isErr()).toBe(true);
+    if (result.isErr()) {
+      expect(result.error).toBeInstanceOf(Error);
+      expect(result.error.message).toBe("Invalid date string");
+    }
+  });
+
+  it("無効な日付文字列の場合はエラーを返す 2", () => {
+    const result = convertToDate("");
+    expect(result.isErr()).toBe(true);
+    if (result.isErr()) {
+      expect(result.error).toBeInstanceOf(Error);
+      expect(result.error.message).toBe("Invalid date string");
+    }
+  });
+});
