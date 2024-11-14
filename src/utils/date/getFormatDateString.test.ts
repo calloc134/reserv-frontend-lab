bd460053f91429c1a897f98843fc1d835a9e56c3
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-11-14 23:47:18 +0900

    フォーマット関数 コード修正とテスト充填

--- src/utils/date/getFormatDateString.test.ts
+++ src/utils/date/getFormatDateString.test.ts
@@ -1,5 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { getFormatDateString } from "./getFormatDateString";
+import {
+  getFormatDateString,
+  getFormatDateStringWithTime,
+  getFormatDateStringWithTimeInBrasil,
+} from "./getFormatDateString";
 import { convertToDate } from "./convertToDate";
 
 describe("getFormatDateString", () => {
@@ -7,5 +11,16 @@ describe("getFormatDateString", () => {
     const date = convertToDate("2024-10-15")._unsafeUnwrap();
     const formattedDate = getFormatDateString(date);
     expect(formattedDate).toBe("10月15日(火)");
+  });
+  it("任意の日付を指定した場合、x月y日(曜日) HH:mmの形式で日付を返す", () => {
+    const date = new Date(1727708400000);
+    const formattedDate = getFormatDateStringWithTime(date);
+    expect(formattedDate).toBe("10月1日(火) 00:00");
+  });
+  it("任意の日付を指定した場合、x月y日(曜日) HH:mmの形式で日付を返す ブラジル時間", () => {
+    // この場合、epoch時間は1727751600000となる
+    const date = new Date(1727751600000);
+    const formattedDate = getFormatDateStringWithTimeInBrasil(date);
+    expect(formattedDate).toBe("10月1日(火) 00:00");
   });
 });
