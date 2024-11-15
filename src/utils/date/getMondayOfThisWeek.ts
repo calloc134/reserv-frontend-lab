fa70ae30556cc5d7bb6e9c7ab360c4fd9b5779aa
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-11-15 15:21:19 +0900

    月曜日取得関数のテスト記述

--- src/utils/date/getMondayOfThisWeek.ts
+++ src/utils/date/getMondayOfThisWeek.ts
@@ -1,35 +1,12 @@
+import { previousMonday, nextMonday, isWeekend } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
+
 export function getMondayOfThisWeek(date: Date): Date {
-  // まず本日の曜日を取得
-  // 今日が平日であれば、前の月曜日から次の金曜日までの予約を取得
-  // 今日が休日であれば、次の月曜日から金曜日までの予約を取得
-  // 月曜さえ取得できれば、あとは+4日すれば金曜日になる
-
-  const day = date.getDay();
-
-  let start_date: Date;
-
-  if (day === 0) {
-    // 日曜日
-    start_date = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate() + 1
-    );
-  } else if (day === 6) {
-    // 土曜日
-    start_date = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate() + 2
-    );
+  if (isWeekend(toZonedTime(date, "Asia/Tokyo"))) {
+    // 休日であるため、次の月曜日を取得
+    return nextMonday(date);
   } else {
-    // 平日であるため、月曜日を取得
-    start_date = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate() - day + 1
-    );
+    // 平日であるため、直前の月曜日を取得
+    return previousMonday(date);
   }
-
-  return start_date;
 }
