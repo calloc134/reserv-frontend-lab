7f527abe8c937fc27880e73f33bc848532ef4bc2
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-02-02 19:02:40 +0900

    リンクコンポーネントを受け取るようにしてみた

--- src/components/DatePaginator.tsx
+++ src/components/DatePaginator.tsx
@@ -1,49 +1,53 @@
 import { Button } from "./ui/button";
 
 export const DatePaginator = ({
-  onClickPrevious,
-  onClickNext,
+  PreviousLink,
+  NextLink,
 }: {
-  onClickPrevious: () => void;
-  onClickNext: () => void;
+  PreviousLink: (children: React.ReactNode) => React.ReactNode;
+  NextLink: (children: React.ReactNode) => React.ReactNode;
 }) => {
   return (
     <>
-      <Button variant={"secondary"} onClick={onClickPrevious}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-caret-left"
-          width="44"
-          height="44"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="#2c3e50"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M14 6l-6 6l6 6v-12" />
-        </svg>
-      </Button>
+      <PreviousLink>
+        <Button variant={"secondary"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-caret-left"
+            width="44"
+            height="44"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M14 6l-6 6l6 6v-12" />
+          </svg>
+        </Button>
+      </PreviousLink>
       <h1 className="text-3xl font-bold text-gray-800">予約一覧(週)</h1>
-      <Button variant={"secondary"} onClick={onClickNext}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-caret-right"
-          width="44"
-          height="44"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="#2c3e50"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M10 18l6 -6l-6 -6v12" />
-        </svg>
-      </Button>
+      <NextLink>
+        <Button variant={"secondary"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-caret-right"
+            width="44"
+            height="44"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 18l6 -6l-6 -6v12" />
+          </svg>
+        </Button>
+      </NextLink>
     </>
   );
 };
